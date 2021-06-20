4d8e63e2a29960cee65bc92f833688ab1e5b1564
Павел Кулёв <Pashakul24@gmail.com>
2021-06-20 17:18:47 +0400

    Added searchparams class

---
+++ SearchSystemCoreApi/SearchParams.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchSystemCoreApi
+{
+    public class SearchParams
+    {
+        [FromQuery(Name = "wait")]
+        public int WaitTime { get; set; }
+
+        [FromQuery(Name = "randomMin")]
+        public int RandomMin { get; set; }
+
+        [FromQuery(Name = "randomMax")]
+        public int RandomMax { get; set; }
+    }
+}
