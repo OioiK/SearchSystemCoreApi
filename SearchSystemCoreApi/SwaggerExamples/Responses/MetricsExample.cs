87461a019abd4de1792b8da3f5dc856067e88261
Павел Кулёв <Pashakul24@gmail.com>
2021-06-20 17:40:00 +0400

    Added swagger examples

---
+++ SearchSystemCoreApi/SwaggerExamples/Responses/MetricsExample.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+namespace SearchSystemCoreApi.SwaggerExamples.Responses
+{
+    internal class MetricsExample : IExamplesProvider<string>
+    {
+        public string GetExamples()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                var firstNumber = i;
+                var secondNumber = i + 1;
+
+                sb.Append($"{firstNumber}-{secondNumber} сек:\n");
+                sb.Append($"External A --> 1\n");
+                sb.Append($"External B --> 3\n");
+                sb.Append($"External C --> 2\n");
+                sb.Append($"External D --> 0\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SearchSystemCoreApi/SwaggerExamples/Responses/ResponseModelExample.cs
@@ -0,0 +1,33 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using System.Collections.Generic;
+
+namespace SearchSystemCoreApi.SwaggerExamples.Responses
+{
+    internal class ResponseModelExample : IExamplesProvider<List<ResponseModel>>
+    {
+        public List<ResponseModel> GetExamples()
+        {
+            return new List<ResponseModel>()
+            {
+                new ResponseModel()
+                {
+                    Name = "External A",
+                    StatusCode = "OK",
+                    Time = "234 ms"
+                },
+                new ResponseModel()
+                {
+                    Name = "External B",
+                    StatusCode = "OK",
+                    Time = "256 ms"
+                },
+                new ResponseModel()
+                {
+                    Name = "External C",
+                    StatusCode = "ERROR",
+                    Time = "231 ms"
+                }
+            };
+        }
+    }
+}
